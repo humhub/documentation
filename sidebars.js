dd0b26aadbaeaddf34d8598bce9ee9d24fb4831c
Lucas Bartholemy <lucas@bartholemy.com>
2020-08-24 20:43:48 +0200

    Added link to REST Api

--- sidebars.js
+++ sidebars.js
@@ -119,6 +119,18 @@ module.exports = {
             'develop/security',
             'develop/console',
             'develop/oembed',
+        ],
+        'Rest API': [
+            {
+                type: 'link',
+                label: 'Documentation',
+                href: 'https://www.humhub.com/en/marketplace/rest/#Manual'
+            },
+            {
+                type: 'link',
+                label: 'GitHub Repository',
+                href: 'https://github.com/humhub/humhub-modules-rest'
+            }
         ]
     },
     Theming: {
