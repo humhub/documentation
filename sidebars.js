a7f792eb4a49ac423e638d4559f9e22b0408fa4c
Lucas Bartholemy <lucas@bartholemy.com>
2023-03-10 18:51:05 +0100

    adde v.14 release notes

--- sidebars.js
+++ sidebars.js
@@ -16,6 +16,7 @@ module.exports = {
             label: 'Release Notes',
             items: [
                 'about/releasenotes/release_notes',
+                'about/releasenotes/release_notes_1_14',
                 'about/releasenotes/release_notes_1_13',
                 'about/releasenotes/release_notes_1_12',
                 'about/releasenotes/release_notes_1_11',
