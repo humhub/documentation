e47d8ce089eb8f7b8eec2d09a1ce22c3b3301b0f
Lucas Bartholemy <luke-@users.noreply.github.com>
2025-01-13 12:40:01 +0100

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -16,6 +16,7 @@ module.exports = {
             label: 'Release Notes',
             items: [
                 'about/releasenotes/release_notes',
+                'about/releasenotes/release_notes_1_17',
                 'about/releasenotes/release_notes_1_16',
                 'about/releasenotes/release_notes_1_15',
                 'about/releasenotes/release_notes_1_14',
